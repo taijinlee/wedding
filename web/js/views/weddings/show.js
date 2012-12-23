4e1e5006833637928375243f41fe22f061558855
Tai-Jin Lee <taijin@gmail.com>
2012-12-23 08:03:28 -0800

    adding option to delete party

--- web/js/views/weddings/show.js
+++ web/js/views/weddings/show.js
@@ -9,18 +9,24 @@ define([
 ], function($, _, Backbone, PartysCollection, emptyTableTemplate, partyRowTemplate, addPartyRowTemplate) {
 
   var View = Backbone.View.extend({
+    events: {
+      'click .delete': 'deleteParty'
+    },
+
     initialize: function(config, vent, pather, cookie, args) {
       this.config = config; this.vent = vent; this.pather = pather; this.cookie = cookie;
       this.weddingId = args[0];
       this.partys = new PartysCollection();
       this.partys.on('reset', this.renderPartys, this);
+      this.partys.on('remove', this.removeParty, this);
 
       this.headers = [
         'Guest Name',
         'E-mail',
         'Address',
         "RSVP'ed",
-        'Edit'
+        'Edit',
+        'Delete'
       ];
     },
 
@@ -43,10 +49,31 @@ define([
       var $body = this.$el.find('tbody');
       var self = this;
       partys.each(function(party) {
-        $body.append(_.template(partyRowTemplate, { guests: party.get('guests'), address: party.address(), editUrl: self.pather.getUrl('partyEdit', { weddingId: self.weddingId, partyId: party.get('id') }) }));
+        var templateVars = {
+          guests: party.get('guests'),
+          address: party.address(),
+          partyId: party.get('id'),
+          editUrl: self.pather.getUrl('partyEdit', { weddingId: self.weddingId, partyId: party.get('id') }),
+        };
+        $body.append(_.template(partyRowTemplate, templateVars));
       });
       $body.append(_.template(addPartyRowTemplate, { addPartyUrl: this.pather.getUrl('partyNew', { id: this.weddingId }) }));
+    },
+
+    deleteParty: function(event) {
+      event.preventDefault() && event.stopPropagation();
+      var partyId = $(event.currentTarget).closest('tr').data('party-id');
+      if (confirm('Delete party?')) {
+        var party = this.partys.get(partyId);
+        party.destroy();
+        this.partys.remove(party);
+      }
+    },
+
+    removeParty: function(model, collection, options) {
+      $('tr[data-party-id="' + model.get('id') + '"]').remove();
     }
+
   });
 
   return View;
