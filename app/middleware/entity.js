fde076b2a2b37704b56f73e94242fdaf61f8f40d
Roger Hsiao <ryhsiao@ryhsiao-ER883AA-ABA-m7470n.(none)>
2012-12-28 14:52:02 -0800

    Refactored exists into 2 separate functions

--- app/middleware/entity.js
+++ app/middleware/entity.js
@@ -1,34 +1,45 @@
 
 module.exports = function(store, cookieJar) {
 
-  var exists = function(modelName, param, key, negate) {
+  var exists = function(modelName, param, key) {
     return function(req, res, next) {
       param = param || (modelName + 'Id');
       key = key || 'id';
       var modelKey = req.param(param, null);
       if (!modelKey) { return next(new Error('notFound: ' + modelName + ':' + key + ' expected in parameters')); }
-
-      var criteria = {};
-      criteria[key] = modelKey;
-
-      var Model = require(process.env.APP_ROOT + '/models/' + modelName + '.js')(store);
-
-      new Model(criteria).retrieve(function(error, modelData) {
-        if (error) { return next(error); }
-        if (!negate) {
-            if (!modelData) { return next(new Error('notFound: ' + modelName + ':' + key + '=' + modelKey + ' does not exist')); }
-        } else {
-          if (modelData) { return next(new Error('alreadyExists: ' + modelName + ':' + key + ':' + modelKey)); }
-        }
-
+        
+      fetch(key, modelKey, modelName, function(error, modelData) {
+        if (error) {return next(error)};
+        if (!modelData) { return next(new Error('notFound: ' + modelName + ':' + key + '=' + modelKey + ' does not exist')); }
         return next();
       });
-
     };
   };
 
+  var notExists = function(modelName, param, key) {
+    return function(req, res, next) {
+      param = param || (modelName + 'Id');
+      key = key || 'id';
+      var modelKey = req.param(param, null);
+      if (!modelKey) { return next(new Error('notFound: ' + modelName + ':' + key + ' expected in parameters')); }
+      fetch(key, modelKey, modelName, function(error, modelData) {
+        if (error) {return next(error)};
+        if (modelData) { return next(new Error('alreadyExists: ' + modelName + ':' + key + ':' + modelKey)); }
+        return next();
+      });
+    };
+  }; 
+
+  var fetch = function(key, modelKey, modelName, callback) {
+    var criteria = {};
+    criteria[key] = modelKey;
+    var Model = require(process.env.APP_ROOT + '/models/' + modelName + '.js')(store);
+    new Model(criteria).retrieve(callback); 
+  };                                                         
+
   return {
-    exists: exists
+    exists: exists,
+    notExists : notExists
   };
 
 };
