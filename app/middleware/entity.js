675350032afdbf714fdbb5574d5f2621878b2c66
Roger Hsiao <ryhsiao@ryhsiao-ER883AA-ABA-m7470n.(none)>
2012-12-28 15:06:13 -0800

    Fixed semicolons. Cleared whitespace

--- app/middleware/entity.js
+++ app/middleware/entity.js
@@ -7,9 +7,8 @@ module.exports = function(store, cookieJar) {
       key = key || 'id';
       var modelKey = req.param(param, null);
       if (!modelKey) { return next(new Error('notFound: ' + modelName + ':' + key + ' expected in parameters')); }
-        
       fetch(key, modelKey, modelName, function(error, modelData) {
-        if (error) {return next(error)};
+        if (error) {return next(error); }
         if (!modelData) { return next(new Error('notFound: ' + modelName + ':' + key + '=' + modelKey + ' does not exist')); }
         return next();
       });
@@ -23,7 +22,7 @@ module.exports = function(store, cookieJar) {
       var modelKey = req.param(param, null);
       if (!modelKey) { return next(new Error('notFound: ' + modelName + ':' + key + ' expected in parameters')); }
       fetch(key, modelKey, modelName, function(error, modelData) {
-        if (error) {return next(error)};
+        if (error) { return next(error); }
         if (modelData) { return next(new Error('alreadyExists: ' + modelName + ':' + key + ':' + modelKey)); }
         return next();
       });
@@ -35,11 +34,10 @@ module.exports = function(store, cookieJar) {
     criteria[key] = modelKey;
     var Model = require(process.env.APP_ROOT + '/models/' + modelName + '.js')(store);
     new Model(criteria).retrieve(callback); 
-  };                                                         
+  };
 
   return {
     exists: exists,
     notExists : notExists
   };
-
 };
