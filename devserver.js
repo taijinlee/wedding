56dbba9088c4de9e8f666bceb4b2fe253896d430
Roger Hsiao <roger@apricotwhisk.com>
2013-01-10 21:48:57 -0800

    Removed hash versioning from devserver.js #4

--- devserver.js
+++ devserver.js
@@ -6,22 +6,17 @@ http.createServer(function (req, res) {
   if (req.method === 'POST') {
     exec('git rev-parse --verify HEAD', function(error, stdout, stderror) {
       if (error) { return console.log(error); }
-      var latestHash = stdout.trim();
-      config.dir += '/' + latestHash;
         
       requirejs.optimize(config, function (buildResponse) {
         console.log(buildResponse);
-        var versionedFiles = [config.dir + 'js/main.js', config.dir + 'layout.html'].join(' ');
-            
-        exec('find ' + versionedFiles + " -exec sed -i -e 's/VERSION/" + latestHash + "/g' {} \\;", function(error, stdout, stderror) {
+
+        exec('make dev', function(error, stdout, stderror) {
           if (error) { return console.log(error); }
-          console.log(versionedFiles + ' have VERSION substituted with ' + latestHash);
         });
       });
     });
   } 
   res.writeHead(200, {'Content-Type': 'text/plain'});
-
   res.end('Done');
 
 }).listen(2222);
