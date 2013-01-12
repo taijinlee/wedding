29abd63aab49ffa32b432c2a10fc630987f84cf5
Roger Hsiao <roger@apricotwhisk.com>
2013-01-12 00:49:13 -0800

    Modified devserver.js to use git pull --rebase instead of verifying the HEAD Also fixed extraneous indentation #4

--- devserver.js
+++ devserver.js
@@ -6,17 +6,17 @@ var http = require('http');
 http.createServer(function (req, res) {
   console.log(req);
   if (req.method === 'POST') {
-    exec('git rev-parse --verify HEAD', function(error, stdout, stderror) {
+    exec('git pull --rebase', function(error, stdout, stderror) {
       if (error) { return console.log(error); }
-        console.log("Git pulled latest code");
-        exec('./node_modules/forever/bin/forever stop app/server.js', function(error, stdout, stderror) {
+      console.log("Git pulled latest code");
+      exec('./node_modules/forever/bin/forever stop app/server.js', function(error, stdout, stderror) {
+        if (error) { return console.log(error); }
+        console.log("forever stopped app/server.js");
+	exec('./node_modules/forever/bin/forever start -l /service/log/forever.log -o /service/log/app.log -e /service/log/app-stderr.log -p /service/tmp --append app/server.js', function (error, stdout, stderror) {
           if (error) { return console.log(error); }
-            console.log("forever stopped app/server.js");
-	    exec('./node_modules/forever/bin/forever start -l /service/log/forever.log -o /service/log/app.log -e /service/log/app-stderr.log -p /service/tmp --append app/server.js', function (error, stdout, stderror) {
-              if (error) { return console.log(error); }
-              console.log("forever started app/server.js");
-            });
+          console.log("forever started app/server.js");
         });
+      });
     });
   } 
   res.writeHead(200, {'Content-Type': 'text/plain'});
