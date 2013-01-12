227bddef545651651ca284052c7aee493759f1c8
Tai-Jin Lee <taijin@apricotwhisk.com>
2013-01-12 11:46:51 -0800

    some stylistic changes, fixing return on callbacks, adding missing semicolons #4

--- devserver.js
+++ devserver.js
@@ -2,35 +2,34 @@ var async = require('async');
 var exec = require('child_process').exec;
 var http = require('http');
 
-http.createServer(function (req, res) {
+http.createServer(function(req, res) {
   if (req.method === 'POST') {
     async.series([
       function(callback) {
         exec('git pull --rebase', function(error, stdout, stderror) {
-          if (error) { callback(error); }
+          if (error) { return callback(error); }
           console.log("Git pulled latest code");
-            callback(null);
-        })
+          return callback(null);
+        });
       },
       function(callback) {
         exec('./node_modules/forever/bin/forever stop app/server.js', function(error, stdout, stderror) {
-          if (error) { callback(error); }
+          if (error) { return callback(error); }
           console.log("forever stopped app/server.js");
-          callback(null);
-        })
+          return callback(null);
+        });
       },
       function(callback) {
         exec('./node_modules/forever/bin/forever start -l /service/log/forever.log -o /service/log/app.log -e /service/log/app-stderr.log -p /service/tmp --append app/server.js', function (error, stdout, stderror) {
-          if (error) { callback(error); }
+          if (error) { return callback(error); }
           console.log("forever started app/server.js");
-          callback(null);
-        })
+          return callback(null);
+        });
       },
-    ], 
-    function (error, result) {
-        if (error) { console.log(error); }
+    ], function (error, result) {
+      if (error) { console.log(error); }
     });
-  } 
+  }
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Done');
 }).listen(2222);
