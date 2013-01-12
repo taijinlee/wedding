7c763ef92214807cb7671c2cd2bd1045fce6b80f
Roger Hsiao <roger@apricotwhisk.com>
2013-01-12 03:08:34 -0800

    Used async to make code less nested. Changed log message to indicate devserver. #4

--- devserver.js
+++ devserver.js
@@ -1,25 +1,38 @@
-var requirejs = require('requirejs');
-var fs = require('fs');
+var async = require('async');
 var exec = require('child_process').exec;
-
 var http = require('http');
+
 http.createServer(function (req, res) {
   console.log(req);
   if (req.method === 'POST') {
-    exec('git pull --rebase', function(error, stdout, stderror) {
-      if (error) { return console.log(error); }
-      console.log("Git pulled latest code");
-      exec('./node_modules/forever/bin/forever stop app/server.js', function(error, stdout, stderror) {
-        if (error) { return console.log(error); }
-        console.log("forever stopped app/server.js");
-	exec('./node_modules/forever/bin/forever start -l /service/log/forever.log -o /service/log/app.log -e /service/log/app-stderr.log -p /service/tmp --append app/server.js', function (error, stdout, stderror) {
-          if (error) { return console.log(error); }
+    async.series([
+      function() {
+        exec('git pull --rebase', function(error, stdout, stderror) {
+          if (error) { callback(error); }
+          console.log("Git pulled latest code");
+            callback(null);
+        })
+      },
+      function(callback) {
+        exec('./node_modules/forever/bin/forever stop app/server.js', function(error, stdout, stderror) {
+          if (error) { callback(error); }
+          console.log("forever stopped app/server.js");
+          callback(null);
+        })
+      },
+      function(callback) {
+        exec('./node_modules/forever/bin/forever start -l /service/log/forever.log -o /service/log/app.log -e /service/log/app-stderr.log -p /service/tmp --append app/server.js', function (error, stdout, stderror) {
+          if (error) { callback(error); }
           console.log("forever started app/server.js");
-        });
-      });
+          callback(null);
+        })
+      },
+    ], 
+    function (error, result) {
+        if (error) { console.log(error); }
     });
   } 
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Done');
 }).listen(2222);
-console.log('Server running on port 2222');
+console.log('DevServer running on port 2222');
