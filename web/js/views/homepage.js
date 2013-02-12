fb7ddddd62ea9897ee9094061767f313aa7ceedd
Tai-Jin Lee <taijin@apricotwhisk.com>
2013-02-12 09:29:58 -0800

    making homepage redirect to guestlist if loggedin

--- web/js/views/homepage.js
+++ web/js/views/homepage.js
@@ -7,6 +7,9 @@ define([
   var View = Backbone.View.extend({
     initialize: function(config, vent, pather, cookie, args) {
       this.config = config; this.vent = vent; this.pather = pather; this.cookie = cookie;
+      if (this.cookie.get('userId')) {
+        return Backbone.history.navigate(this.pather.getUrl('userGuestlist'), { trigger: true });
+      }
     },
 
     render: function() {
