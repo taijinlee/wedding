3197c1a2ac9b34468af84939767ef680cd6a8b26
Roger Hsiao <roger@apricotwhisk.com>
2013-02-15 16:56:33 -0800

    Changed address to checkbox #67

--- web/js/views/guestlist/partysTable/addressed.js
+++ web/js/views/guestlist/partysTable/addressed.js
@@ -2,12 +2,12 @@ define([
   'jquery',
   'underscore',
   'backbone',
-  'text!./addressed.html'
+  'text!../addressed.html'
 ], function($, _, Backbone, addressedTemplate) {
 
   var View = Backbone.View.extend({
     events: {
-      'click .addressedSelector': 'setAddressed'
+      'click .checkbox': 'setAddressed'
     },
 
     initialize: function(config, vent, pather, cookie, party) {
@@ -16,23 +16,21 @@ define([
     },
 
     render: function() {
-      var isAddressed = (this.party.get('isAddressed')) ? 'y' : 'n';
+      var isAddressed = (this.party.get('isAddressed')) ? true : false;
       this.$el.html(_.template(addressedTemplate, { isAddressed: isAddressed }));
       return this;
     },
 
     setAddressed: function(event) {
       event.preventDefault(); event.stopPropagation();
-      var $addressedButton = $(event.target);
-
+      var $addressedCheckBox = $(event.target);
       var addressed = false;
-      if (!$addressedButton.hasClass('btn-on')) {
-        addressed = ($addressedButton.data('addressed') === 'y') ? true : false; // only need button priority if we're switching to on
+
+      if ($addressedCheckBox.prop('checked')) {
+        addressed = true;
       }
       this.party.set({ isAddressed: addressed }).save({}, {
         success: function() {
-          $addressedButton.siblings().removeClass('btn-on');
-          $addressedButton.addClass('btn-on');
         }
       });
 
